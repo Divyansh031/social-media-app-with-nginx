724c25e1b87a67ff8461fb05dc3fc262946d3e17
Divyansh Sharma <divyanshsayz@gmail.com>
2025-07-31 11:07:37 +0530

    Added CORS and requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
-from .config import settings
 
 # models.Base.metadata.create_all(bind=engine) Since now we have ALEMBIC for migrations
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include routers for posts, users and authentication
 app.include_router(post.router)
@@ -16,6 +26,8 @@ app.include_router(vote.router)
 
 
 
-
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
 
 
